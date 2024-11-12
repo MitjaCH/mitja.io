48fe44f527eb2f44294d9499f7a0fbc3551aca60
MitjaCH <mitja.kurath@mitja.io>
2024-11-12 20:32:12 +0100

    Removed about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, ProjectsComponent, ContactComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, ProjectsComponent, ContactComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
