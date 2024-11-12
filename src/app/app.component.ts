36f9a390e93425aae7b68980e6f35717cc3c007e
MitjaCH <mitja.kurath@mitja.io>
2024-11-12 20:29:59 +0100

    Added navbar component + created header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, ProjectsComponent, ContactComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}"
   ],
   theme: {
     extend: {
-      colors: {
-        glass: 'rgba(255, 255, 255, 0.15)',
-      },
-      backdropBlur: {
-        xs: '2px',
-      },
+      colors: {}
     },
   },
   plugins: [],
