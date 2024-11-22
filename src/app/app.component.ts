96c28d95791c1529cf48545c004cad97b124c5ab
Mitja Kurath <123660565+MitjaCH@users.noreply.github.com>
2024-11-22 10:24:12 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProjectsComponent, ContactComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, ProjectsComponent, ContactComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
